715d268deda5b8a284a6b3c29e4db8d12cf717bb
Your Name <you@example.com>
2017-01-28 13:16:29 -0700

    Master-Detail no refactor

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,26 @@
+/*
+Defines AppModule, the root module that tells Angular how to assemble the application. 
+Right now it declares only the AppComponent. 
+Soon there will be more components to declare. 
+*/
+// Default getstarted modules
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+// Additional modules ...
+import { FormsModule }   from '@angular/forms';
 
+// Default getstarted modules
 import { AppComponent }  from './app.component';
 
+// Add it to the NgModule decorator's 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  declarations: [
+    AppComponent
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- app/main.ts
+++ app/main.ts
@@ -1,3 +1,6 @@
+/*
+Compiles the application with the JIT compiler and bootstraps the application to run in the browser. That's a reasonable choice for the development of most projects and it's the only viable choice for a sample running in a live-coding environment like Plunker. You'll learn about alternative compiling and deployment options later in the documentation.
+*/
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
