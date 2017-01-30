bf1029ba62d30ef2330e010c9a1cd358b4aff009
ivanchenoweth@gmailcom <ivanchenoweth@gmailcom>
2017-01-29 18:07:04 -0700

    master-detail

--- app/app.module.ts
+++ app/app.module.ts
@@ -6,7 +6,10 @@ Soon there will be more components to declare.
 // Default getstarted modules
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+<<<<<<< HEAD
+=======
 // Additional modules ...
+>>>>>>> 715d268deda5b8a284a6b3c29e4db8d12cf717bb
 import { FormsModule }   from '@angular/forms';
 
 // Default getstarted modules
@@ -23,4 +26,15 @@ import { AppComponent }  from './app.component';
   ],
   bootstrap: [ AppComponent ]
 })
+<<<<<<< HEAD
 export class AppModule { }
+
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
+=======
+export class AppModule { }
+>>>>>>> 715d268deda5b8a284a6b3c29e4db8d12cf717bb
