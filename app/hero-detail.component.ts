bf30ea859df0fe8de80efdd04bb0a11e28678e78
ivanchenoweth@gmailcom <ivanchenoweth@gmailcom>
2017-02-05 11:44:07 -0700

    routing is not working ...

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -6,10 +6,9 @@ import { Location }               from '@angular/common';
 import { Hero }         from './hero';
 import { HeroService }  from './hero.service';
 @Component({
-  moduleId: module.id,
   selector: 'my-hero-detail',
-  templateUrl: './hero-detail.component.html',
-  styleUrls: [ './hero-detail.component.css' ]
+  templateUrl: 'app/hero-detail.component.html',
+  styleUrls: [ 'app/hero-detail.component.css' ]
 })
 export class HeroDetailComponent implements OnInit {
   hero: Hero;
